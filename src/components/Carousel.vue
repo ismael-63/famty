95ee3df660f26d2b6c144435e971c6e52752e9ec
Ismael <islaral63@gmail.com>
2026-03-15 16:12:20 +0100

    Carrousel convertido a un componente de vue

---
+++ src/components/Carousel.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="d-flex carousel-container">
+    <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="4000">
+          <img src="/src/assets/img/fondo.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="1000">
+          <img src="/src/assets/img/algo.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="1000">
+          <img src="/src/assets/img/pinto.jpg" class="d-block w-100" alt="...">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#carouselId {
+  width: 100vw;
+  margin-left: calc(-50vw + 50%);
+  margin-right: calc(-50vw + 50% - 2rem);
+}
+
+.carousel-item {
+  width: 100%;
+  height: 60vh;
+}
+
+.carousel-item img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  object-position: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,12 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Carousel from '../components/Carousel.vue'
 import { onMounted } from 'vue'
 
 </script>
 
 <template>
-  <div class="d-flex carousel-container">
-    <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="4000">
-          <img src="/src/assets/img/fondo.png" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item" data-bs-interval="1000">
-          <img src="/src/assets/img/algo.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item" data-bs-interval="1000">
-          <img src="/src/assets/img/pinto.jpg" class="d-block w-100" alt="...">
-        </div>
-      </div>
-    </div>
-  </div>
+  <Carousel />
 
   <div class="information-container">
     <div container-fluid class="d-flex justify-center align-middle text-center">
@@ -38,25 +25,5 @@ import { onMounted } from 'vue'
 .information-container{
   margin-top: 2vh;
   background-color: black;
-}
-
-#carouselId {
-  width: 100vw;
-  margin-left: calc(-50vw + 50%);
-  margin-right: calc(-50vw + 50% - 2rem);
-
-}
-
-.carousel-item {
-  width: 100%;
-  height: 60vh;
-}
-
-.carousel-item img {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-  object-position: center;
-
 }
 </style>
