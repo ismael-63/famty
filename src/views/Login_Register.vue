b4f6ae3f96827b81b54487d669e09a677282a083
Ismael <islaral63@gmail.com>
2026-03-22 12:43:02 +0100

    div centrado para el formulario de login

--- src/views/Login_Register.vue
+++ src/views/Login_Register.vue
@@ -3,13 +3,15 @@
 </script>
 
 <template>
-
-<div class="container d-flex justify-content-center bg-primary">
-    <p>Hola</p>
-</div>
-
+    <div class="container d-flex justify-content-center align-items-center login_div">
+        <div class="bg-white p-5 rounded shadow">
+            <p>hola</p>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-
+.login_div{
+    min-height: 50vw;
+}
 </style>
